7a7eb2a0a55b6963aac60f16a2776cf01f1f46c8
youngdo cho <youngdocho@p-ful.com>
2016-09-01 12:26:27 +0900

    modified for galera nodes

--- puppet.sh
+++ puppet.sh
@@ -3,3 +3,7 @@ cp -r /puppet/testrepo/modules/* /etc/puppet/modules
 ln -s /puppet/testrepo/ /etc/puppet/modules/galera/
 echo 'user localhost=(root) NOPASSWD: /sbin/iptables -vnxL' >> /etc/sudoers
 puppet apply /puppet/testrepo/manifests/nodes.pp --hiera_config=/puppet/testrepo/hiera.yaml --trace
+
+if hostname | grep ^node* > /dev/true; then
+  service xinetd start
+fi 
