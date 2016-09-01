88bc7918b8fa80ded1a8a59678bbce83308de1c1
youngdo cho <youngdocho@p-ful.com>
2016-09-01 11:16:30 +0900

    sh script added

---
+++ puppet.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp -r /puppet/testrepo/modules/* /etc/puppet/modules
+ln -s /puppet/testrepo/ /etc/puppet/modules/galera/
+echo 'user localhost=(root) NOPASSWD: /sbin/iptables -vnxL' >> /etc/sudoers
+puppet apply /puppet/testrepo/manifests/nodes.pp --hiera_config=/puppet/testrepo/hiera.yaml --trace
