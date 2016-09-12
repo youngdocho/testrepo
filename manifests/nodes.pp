58e2a6645ccbfafaa712082960c030fd89f767b8
Youngdo Cho <youngdocho@p-ful.com>
2016-09-12 14:18:39 +0900

    cronjob.pp created

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,20 +1,5 @@
-define cron_job (
-  $script_path,
-  $minute = '*/10',  
-) {
-  exec { 'script-copy':
-    command => "/bin/cp ${script_path} /usr/local/sbin/${name}",    
-    ensure  => present,
-  }
-  cron { "Run ${name}":
-    command => "/usr/local/sbin/${name}",
-    user    => 'root',
-    minute  => $minute',
-  }
-}
-
 node /node.*/ {
- cron_job { 'pull-updates':
+ admin::cronjob { 'pull-updates':
     script_path => '/puppet/testrepo/pull-updates',
  }
  class { 'galera':
@@ -31,7 +16,7 @@ node /node.*/ {
 }
 
 node /garb.*/ {
- cron_job { 'pull-updates':
+ admin::cronjob { 'pull-updates':
     script_path => '/puppet/testrepo/pull-updates',
  }
  class {'garb': 
@@ -41,7 +26,7 @@ node /garb.*/ {
 }
 
 node 'haproxy' {
- cron_job { 'pull-updates':
+ admin::cronjob { 'pull-updates':
     script_path => '/puppet/testrepo/pull-updates',
  }
  class { 'haproxy': 
---
+++ modules/admin/manifests/cronjob.pp
@@ -0,0 +1,14 @@
+define admin::cronjob (
+  $script_path,
+  $minute = '*/10',
+) {
+  exec { 'script-copy':
+    command => "/bin/cp ${script_path} /usr/local/sbin/${name}",
+    ensure  => present,
+  }
+  cron { "Run ${name}":
+    command => "/usr/local/sbin/${name}",
+    user    => 'root',
+    minute  => $minute',
+  }
+}
